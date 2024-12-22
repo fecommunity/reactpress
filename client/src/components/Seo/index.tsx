cc69b953971aaf89c57e4fd951637f430f64e854
m0_37981569 <admin@gaoredu.com>
2024-12-22 21:34:46 +0800

    refactor: update seo meta info

--- client/src/components/Seo/index.tsx
+++ client/src/components/Seo/index.tsx
@@ -11,10 +11,13 @@ export const Seo = () => {
     <Head>
       <title>{setting.systemTitle}{setting.systemSubTitle ? ` - ${setting.systemSubTitle}` : ''}</title>
       <meta name="viewport" content="width=device-width,initial-scale=1.0,viewport-fit=cover,maximum-scale=1" />
+      <meta http-equiv="X-UA-Compatible" content="ie=edge" />
       <meta name="copyright" content="Copyright (c) 2024 ReactPress(https://github.com/fecommunity/reactpress). All rights reserved." />
+      <link rel="shortcut icon" href={setting.systemFavicon} />
+      <link rel="apple-touch-icon" href={setting.systemFavicon}/>
       <meta name="keywords" content={setting.seoKeyword} />
       <meta name="description" content={setting.seoDesc} />
-      <link rel="shortcut icon" href={setting.systemFavicon} />
+      <meta name='robots' content='max-image-preview:large' />
       <link href="//fonts.googleapis.com/css?family=Nunito:400,400i,700,700i&amp;display=swap" rel="stylesheet"></link>
     </Head>
   );
