b583d5385f114e2159355a99f3cd90861181a269
m0_37981569 <admin@gaoredu.com>
2025-03-05 00:10:54 +0800

    feat: add reactpress docs

---
+++ docs/src/pages/index.tsx
@@ -0,0 +1,37 @@
+/**
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+import React from 'react';
+
+import Head from '@docusaurus/Head';
+
+import Layout from '@theme/Layout';
+
+import Home from '@site/src/pages/Home';
+
+const Index = () => {
+  return (
+    <Layout
+      description="A framework for building native apps using React"
+      wrapperClassName="homepage">
+      <Head>
+        <title>ReactPress - 一个基于Next.js的博客&CMS系统。</title>
+        <meta
+          property="og:title"
+          content="React Native · Learn once, write anywhere"
+        />
+        <meta
+          property="twitter:title"
+          content="React Native · Learn once, write anywhere"
+        />
+      </Head>
+      <Home />
+    </Layout>
+  );
+};
+
+export default Index;
