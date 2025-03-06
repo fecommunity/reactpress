7e681e9e065e0cb2b885169d4050bf39e5ecafa0
FECommunity <admin@gaoredu.com>
2025-03-06 23:00:05 +0800

    docs: update docs folder name

--- docs/docusaurus.config.ts
+++ docs/docusaurus.config.ts
@@ -36,6 +36,7 @@ const config: Config = {
       '@docusaurus/preset-classic',
       {
         docs: {
+          path: './tutorial',
           sidebarPath: './sidebars.ts',
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
