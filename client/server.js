2eeef437d6d1be2bbce57219d9244b225b208764
m0_37981569 <admin@gaoredu.com>
2025-09-20 20:58:24 +0800

    chore: update build script

--- client/server.js
+++ client/server.js
@@ -1,11 +1,20 @@
 const { config } = require('@fecommunity/reactpress-config');
-const cli = require('next/dist/cli/next-start');
+const cliProd = require('next/dist/cli/next-start');
+const cliDev = require('next/dist/cli/next-dev');
+const open = require('open');
 
 const port = config.CLIENT_PORT || 3001;
 
 try {
-  cli.nextStart(['-p', port]);
-  console.log(`[reactpress] 客户端已启动，端口：${port}`);
+  // 这里根据环境判断，如果NODE_ENV=production, 就是生产
+  if (process.env.NODE_ENV === 'production') {
+    cliProd.nextStart(['-p', port]);
+  } else {
+    cliDev.nextDev(['-p', port]);
+  }
+  console.log(`[reactpress] 客户端已启动，端口：${port}`, `访问地址：http://localhost:${port}`);
+  // 尝试自动打开
+  open(`http://localhost:${port}`);
 } catch (err) {
   console.log(`[reactpress] 客户端启动失败！${err.message || err}`);
 }
