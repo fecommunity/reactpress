5508479a0d989c1c230e4c7136a94ef387d53d75
m0_37981569 <admin@gaoredu.com>
2026-05-30 10:07:18 +0800

    chore: remove deprecated files and components from Twenty Twenty-Six theme, streamlining the codebase for improved maintainability

--- themes/twentytwentysix/components/chrome/index.ts
+++
@@ -1,3 +0,0 @@
-export { default as ColorSchemeToggle } from './ColorSchemeToggle';
-export { default as HeaderToolbar } from './HeaderToolbar';
-export { default as LocaleToggle } from './LocaleToggle';
--- themes/twentytwentysix/pages/_app.tsx
+++
@@ -1,6 +0,0 @@
-import { createThemeApp } from '@fecommunity/reactpress-toolkit/theme';
-import '../styles/tailwind.css';
-import '../styles/client-theme.css';
-import themeManifest from '../theme.json';
-
-export default createThemeApp(themeManifest);
