8a099aa10cdfb4cb0bc6e2a39c7a46fc36db3683
m0_37981569 <admin@gaoredu.com>
2026-05-30 10:10:48 +0800

    chore: remove Twenty Twenty-Five theme files and components, cleaning up the codebase for better maintainability

--- themes/twentytwentyfive/pages/_app.tsx
+++
@@ -1,5 +0,0 @@
-import { createThemeApp } from '@fecommunity/reactpress-toolkit/theme';
-import '../styles/blog.css';
-import themeManifest from '../theme.json';
-
-export default createThemeApp(themeManifest);
